f6c8540501390d727e5115118ede46bab1f4ddfc
lorenzodianni <lorenzodianni@gmail.com>
2020-11-25 01:02:42 +0100

    test(meso-e2e): should skip login

--- apps/meso-e2e/src/integration/auth/login/login.ts
+++ apps/meso-e2e/src/integration/auth/login/login.ts
@@ -1,13 +1,31 @@
-describe('meso signup', () => {
-  beforeEach(() => cy.visit('/auth/login'));
+import { authLocalStorage, adaptToReduxPersist } from '../../../fixtures/auth/localStorage.mock';
 
-  it('should form submit button to be disabled', () => {
-    cy.get('form button').should('be.disabled');
+const loginPath = '/auth/login';
+
+describe('meso signup', () => {
+  describe('login', () => {
+    beforeEach(() => cy.visit(loginPath));
+
+    it('should form submit button to be disabled', () => {
+      cy.get('form button').should('be.disabled');
+    });
+
+    it('should form submit button to be enabled', () => {
+      cy.get('#auth-email').type('e2e@e2e.eu');
+      cy.get('#auth-password').type('123123');
+      cy.get('form button').should('not.be.disabled');
+    });
   });
 
-  it('should form submit button to be enabled', () => {
-    cy.get('#auth-email').type('e2e@e2e.eu');
-    cy.get('#auth-password').type('123123');
-    cy.get('form button').should('not.be.disabled');
+  describe('skip login page', () => {
+    it('should redirect to feed page if token exist', () => {
+      cy.visit(loginPath, {
+        onBeforeLoad(win) {
+          win.localStorage.setItem('persist:root', adaptToReduxPersist(authLocalStorage));
+        }
+      });
+
+      cy.location('pathname').should('eq', '/');
+    });
   });
 });
